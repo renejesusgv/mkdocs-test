d10da2aa2a2c7457efc1104a4f59a35442dd5b1d
Rene Jesus Gaspar Velasco <rene.gaspar@wizeline.com>
2021-08-12 14:57:35 -0500

    Adds Js scripts folder

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,7 +1,26 @@
-module.exports.toDashCase = function(str){
+const metadataParser = require('markdown-yaml-metadata-parser');
+
+function toDashCase(str){
     dashedString = str.replace(/\s+/g, '-').toLowerCase();
-    console.log(dashedString);
+    //console.log(dashedString);
     return dashedString;
 }
+function buildFileName(str){
+    const parsedMetadata = metadataParser(str).metadata
+    if (parsedMetadata && parsedMetadata.sections.length){
+        roleRegex = /(?<=\s)(.*?)(?=\/)/g;
+        typeRegex = /(?<=\/).*/g;
+        console.log(parsedMetadata.sections[0]);
+        role = parsedMetadata.sections[0].match(roleRegex) ? parsedMetadata.sections[0].match(roleRegex)[0] : "";
+        type = parsedMetadata.sections[0].match(typeRegex) ? parsedMetadata.sections[0].match(typeRegex)[0] : "";
+        resource = toDashCase(parsedMetadata.title);
+        return `${role}.${type}.${resource}.md`.toLowerCase();
+    } else {
+        return "index.md"
+    }
+}
+
+module.exports.toDashCase = toDashCase
+module.exports.buildFileName = buildFileName
 
 //node -e 'require("./scripts/helpers").toDashCase("some string")'
